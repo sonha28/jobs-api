e97471a33f3df5276b38842e0e2978f125e6bdf7
Sơn Hoàng <sonha@Sons-MacBook-Pro.local>
2023-07-14 13:59:24 +0700

    Add swagger

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const rateLimiter = require('express-rate-limit')
 const express = require('express');
 const app = express();
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 // connect DB
 
 const connectDB = require('./db/connect')
@@ -32,6 +37,12 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 // extra packages
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1> <a href="/api-docs">Documentation</a>')
+})
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
